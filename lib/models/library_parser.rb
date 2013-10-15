4e0609de50dad0d4eb182e4a1ab03410b4a6459b
Scott Luptowski <scottluptowski@gmail.com>
2013-10-15 16:40:56 -0400

    much cleaner call method

--- lib/models/library_parser.rb
+++ lib/models/library_parser.rb
@@ -14,13 +14,9 @@ class LibraryParser
       file = file.gsub(' - ','|').gsub(' [','|').gsub(']','|')[0...-5]
       artist, song, genre = file.split('|')
 
-      artist_to_insert = Artist.find(artist)
-      artist_to_insert = artist_to_insert || Artist.new.tap {|a| a.name = artist }
-
+      artist_to_insert = Artist.find(artist) || Artist.new.tap {|a| a.name = artist }
       song_to_insert = Song.new.tap {|s| s.name = song }
-      
-      genre_to_insert = Genre.find(genre)
-      genre_to_insert = genre_to_insert || Genre.new.tap {|g| g.name = genre }
+      genre_to_insert = Genre.find(genre) || Genre.new.tap {|g| g.name = genre }
 
       song_to_insert.genre = genre_to_insert 
       artist_to_insert.add_song(song_to_insert)

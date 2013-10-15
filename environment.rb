806cbd2cc62ba809ed68950a973965ea17d4de67
Scott Luptowski <scottluptowski@gmail.com>
2013-10-15 15:29:18 -0400

    Library parser turns files into objects

--- environment.rb
+++ environment.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'lib/models/artist.rb'
 require_relative 'lib/models/genre.rb'
 require_relative 'lib/models/song.rb'
@@ -6,3 +7,4 @@ require_relative 'lib/models/library_parser.rb'
 parser = LibraryParser.new('/Users/scott1/Development/flatiron_homework/playlister-rb-unit-1/db/data')
 
 parser.call
+binding.pry

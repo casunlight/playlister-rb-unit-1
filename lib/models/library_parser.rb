6e520ff2e6fdee9701d0fbcd35558f815e8b4e66
Scott Luptowski <scottluptowski@gmail.com>
2013-10-15 16:33:55 -0400

    tap method

--- lib/models/library_parser.rb
+++ lib/models/library_parser.rb
@@ -17,10 +17,8 @@ class LibraryParser
       artist_to_insert = Artist.find(artist)
       artist_to_insert = artist_to_insert || Artist.new.tap {|a| a.name = artist }
 
-      song_to_insert = Song.new.tap do |s|
-        s.name = song
-      end
-
+      song_to_insert = Song.new.tap {|s| s.name = song }
+      
       genre_to_insert = Genre.find(genre)
       genre_to_insert = genre_to_insert || Genre.new.tap {|g| g.name = genre }
 
